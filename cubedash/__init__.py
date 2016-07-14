0ee8e6aa84dd762f008c0d35c226a9fb7ab5ae5b
Gregory Raevski <gregory.raevski@gmail.com>
2016-07-14 15:20:26 +1000

    compress results

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -13,10 +13,12 @@ import rasterio.warp
 from cachetools import cached
 
 import flask
+from flask.ext.compress import Compress
 import os
 
 static_prefix = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'frontend')
 app = flask.Flask(__name__, static_path='', static_url_path=static_prefix)
+Compress(app)
 
 
 FIELDS = ['platform', 'instrument', 'product']
