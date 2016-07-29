0d0e2e2e4a24df7e0a68f9d51776e8f66f4ddc7a
Jeremy Hooke <jez@stulk.com>
2016-07-29 12:17:19 +1000

    Fix auto reloading of templates in debug mode

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -16,6 +16,8 @@ from datacube.index import index_connect
 from datacube.model import Range
 from datacube.utils import jsonify_document
 
+DEBUG_MODE = True
+
 _PRODUCT_PREFIX = "/<product>"
 # There's probably a proper flask way to do this.
 API_PREFIX = "/api"
@@ -302,4 +304,5 @@ EODATASETS_LINEAGE_PROPERTY_ORDER = [
 ]
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.jinja_env.auto_reload = DEBUG_MODE
+    app.run(port=8080, debug=DEBUG_MODE)
