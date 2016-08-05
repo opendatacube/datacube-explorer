65781523aabe0f8d29401ff4b122d2397c9c8df9
Jeremy Hooke <jez@stulk.com>
2016-08-05 11:53:45 +1000

    Split out a separate utils module

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -1,5 +1,3 @@
-import collections
-import functools
 import sys
 from datetime import datetime
 from json import dumps as jsonify
@@ -9,16 +7,16 @@ import rasterio.warp
 import shapely.geometry
 import shapely.ops
 from cachetools.func import ttl_cache
-from dateutil import parser, tz
-from dateutil.relativedelta import relativedelta
+from dateutil import tz
 
 from datacube.index import index_connect
 from datacube.model import Range
 from datacube.utils import jsonify_document
 
-app = flask.Flask("cubedash")
+from . import _utils as utils
 
-ACCEPTABLE_SEARCH_FIELDS = ["platform", "instrument", "product"]
+app = flask.Flask("cubedash")
+app.register_blueprint(utils.bp)
 
 # Only do expensive queries "once a day"
 # Enough time to last the remainder of the work day, but not enough to still be there the next morning
@@ -32,64 +30,6 @@ def as_json(o):
 # Thread and multiprocess safe.
 # As long as we don't run queries (ie. open db connections) before forking (hence validate=False).
 index = index_connect(application_name="cubedash", validate_connection=False)
-
-
-@app.template_filter("printable_time")
-def _format_datetime(date):
-    return date.strftime("%Y-%m-%d %H:%M:%S")
-
-
-@app.template_filter("query_value")
-def _format_query_value(val):
-    if isinstance(val, Range):
-        return "{} to {}".format(
-            _format_query_value(val.begin), _format_query_value(val.end)
-        )
-    if isinstance(val, datetime):
-        return _format_datetime(val)
-    return str(val)
-
-
-@app.template_filter("month_name")
-def _format_month_name(val):
-    ds = datetime(2016, int(val), 2)
-    return ds.strftime("%b")
-
-
-@app.template_filter("max")
-def _max_val(ls):
-    return max(ls)
-
-
-def parse_query(request):
-    query = {}
-    for field in ACCEPTABLE_SEARCH_FIELDS:
-        if field in request:
-            query[field] = request[field]
-
-    to_time = parser.parse(request["before"]) if "before" in request else None
-    from_time = parser.parse(request["after"]) if "after" in request else None
-
-    # Default from/to values (a one month range)
-    if not from_time and not to_time:
-        to_time = datetime.now()
-    if not to_time:
-        to_time = from_time + relativedelta(months=1)
-    if not from_time:
-        from_time = to_time - relativedelta(months=1)
-
-    query["time"] = Range(from_time, to_time)
-
-    def range_dodge(val):
-        if isinstance(val, list):
-            return Range(val[0], val[1])
-        else:
-            return Range(val - 0.00005, val + 0.00005)
-
-    if "lon" in request and "lat" in request:
-        query["lon"] = range_dodge(request["lon"])
-        query["lat"] = range_dodge(request["lat"])
-    return query
 
 
 def datasets_union(dss):
@@ -185,7 +125,7 @@ def product_timeline_page(product):
 def product_datasets_page(product):
     args = flask.request.args
     query = {"product": product}
-    query.update(parse_query(args))
+    query.update(utils.parse_query(args))
     # TODO: Add sort option to index API
     datasets = sorted(index.datasets.search_eager(**query), key=lambda d: d.center_time)
     return flask.render_template(
@@ -211,78 +151,12 @@ def platform_page(platform):
 def dataset_page(id_):
     dataset = index.datasets.get(str(id_), include_sources=True)
 
-    ordered_metadata = get_ordered_metadata(dataset.metadata_doc)
+    ordered_metadata = utils.get_ordered_metadata(dataset.metadata_doc)
 
     return flask.render_template(
         "dataset.html", dataset=dataset, dataset_metadata=ordered_metadata
     )
 
-
-def get_ordered_metadata(metadata_doc):
-    def get_property_priority(ordered_properties, keyval):
-        key, val = keyval
-        if key not in ordered_properties:
-            return 999
-        return ordered_properties.index(key)
-
-    # Give the document the same order as eo-datasets. It's far more readable (ID/names first, sources last etc.)
-    ordered_metadata = collections.OrderedDict(
-        sorted(
-            metadata_doc.items(),
-            key=functools.partial(get_property_priority, EODATASETS_PROPERTY_ORDER),
-        )
-    )
-    ordered_metadata["lineage"] = collections.OrderedDict(
-        sorted(
-            ordered_metadata["lineage"].items(),
-            key=functools.partial(
-                get_property_priority, EODATASETS_LINEAGE_PROPERTY_ORDER
-            ),
-        )
-    )
-
-    if "source_datasets" in ordered_metadata["lineage"]:
-        for type_, source_dataset_doc in ordered_metadata["lineage"][
-            "source_datasets"
-        ].items():
-            ordered_metadata["lineage"]["source_datasets"][
-                type_
-            ] = get_ordered_metadata(source_dataset_doc)
-
-    return ordered_metadata
-
-
-EODATASETS_PROPERTY_ORDER = [
-    "id",
-    "ga_label",
-    "ga_level",
-    "product_type",
-    "product_level",
-    "product_doi",
-    "creation_dt",
-    "size_bytes",
-    "checksum_path",
-    "platform",
-    "instrument",
-    "format",
-    "usgs",
-    "rms_string",
-    "acquisition",
-    "extent",
-    "grid_spatial",
-    "gqa",
-    "browse",
-    "image",
-    "lineage",
-    "product_flags",
-]
-EODATASETS_LINEAGE_PROPERTY_ORDER = [
-    "algorithm",
-    "machine",
-    "ancillary_quality",
-    "ancillary",
-    "source_datasets",
-]
 
 if __name__ == "__main__":
     DEBUG_MODE = len(sys.argv) == 2 and sys.argv[1] == "--debug"
