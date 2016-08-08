c9deb86cc93d2ce51b7f88d0714801a18157e174
Jeremy Hooke <jez@stulk.com>
2016-08-08 14:13:37 +1000

    Include indexer information for datasets.

--- cubedash/_utils.py
+++ cubedash/_utils.py
@@ -9,6 +9,7 @@ import logging
 from datetime import datetime
 
 from dateutil import parser
+from dateutil import tz
 from dateutil.relativedelta import relativedelta
 from flask import Blueprint
 
@@ -43,6 +44,36 @@ def _format_month_name(val):
 @bp.app_template_filter('max')
 def _max_val(ls):
     return max(ls)
+
+
+@bp.app_template_filter('timesince')
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+
+    http://flask.pocoo.org/snippets/33/
+    """
+
+    now = datetime.utcnow().replace(tzinfo=tz.tzutc())
+    diff = now - dt
+
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
 
 
 def parse_query(request):
