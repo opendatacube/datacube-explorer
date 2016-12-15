04094a138bc7c2532cbbe763e27763d92ec83b9d
Jeremy Hooke <jez@stulk.com>
2016-12-15 17:00:29 +1100

    Show a list of derived datasets

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -162,6 +162,7 @@ def dataset_page(id_):
         "dataset.html",
         dataset=dataset,
         dataset_metadata=ordered_metadata,
+        derived_datasets=index.datasets.get_derived(str(id_)),
         source_datasets=source_datasets,
     )
 
