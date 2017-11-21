f436a78cc06fe24941abc0d43e43613635406e0e
Alex Vincent <alexander.vincent@ga.gov.au>
2017-11-21 18:35:33 +1100

    Adding setup.py file to assist with install

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+tests_require = ["pylint", "digitalearthau"]
+
+dependency_links = [
+    "git+git://github.com/GeoscienceAustralia/digitalearthau/release/latest#egg=digitalearthau"
+]
+
+setup(
+    name="dea-dashboard",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="https://github.com/data-cube/dea-dashboard",
+    author="Geoscience Australia",
+    packages=find_packages(),
+    install_requires=[
+        "datacube",
+        "cachetools",
+        "flask",
+        "jinja2",
+        "python-dateutil",
+        "shapely",
+    ],
+    tests_require=tests_require,
+    dependency_links=dependency_links,
+)
