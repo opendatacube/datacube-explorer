7ac84928121607917e459e746d22089beadc4285
Alex Vincent <alexander.vincent@ga.gov.au>
2017-11-22 15:54:00 +1100

    Fixes on setup.py file

--- setup.py
+++ setup.py
@@ -2,18 +2,14 @@
 
 from setuptools import find_packages, setup
 
-import versioneer
-
 tests_require = ["pylint", "digitalearthau"]
 
 dependency_links = [
-    "git+git://github.com/GeoscienceAustralia/digitalearthau/release/latest#egg=digitalearthau"
+    "git+git://github.com/GeoscienceAustralia/digitalearthau@develop#egg=digitalearthau"
 ]
 
 setup(
     name="dea-dashboard",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     url="https://github.com/data-cube/dea-dashboard",
     author="Geoscience Australia",
     packages=find_packages(),
