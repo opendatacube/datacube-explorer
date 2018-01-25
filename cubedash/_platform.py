b3c43d25767ac77e0263c5c3d2df12e0ae3d62f9
Jeremy Hooke <jez@stulk.com>
2018-01-25 16:27:16 +1100

    Remove legacy platforms page

--- cubedash/_platform.py
+++ cubedash/_platform.py
@@ -2,29 +2,13 @@ from __future__ import absolute_import
 
 import logging
 
-import flask
-from flask import Blueprint
-
-from cubedash import _product
-from cubedash._model import index
+from flask import Blueprint, redirect, url_for
 
 _LOG = logging.getLogger(__name__)
 bp = Blueprint("platform", __name__, url_prefix="/platform")
 
-_HARD_SEARCH_LIMIT = 500
-
-
-def _timelines_platform(platform_name):
-    for product in index.products.search(platform=platform_name):
-        _LOG.debug("Building timeline for platform product %s", product.name)
-        yield product, _product.timeline_years(1986, product)
-
 
 @bp.route("/<platform_name>")
 def platforms_page(platform_name):
-    return flask.render_template(
-        "platform.html",
-        product_counts=_timelines_platform(platform_name),
-        products=[p.definition for p in index.datasets.types.get_all()],
-        platform=platform_name,
-    )
+    # Legacy platform page. Redirect to list of timelines in about page.
+    return redirect(url_for("about_page"))
