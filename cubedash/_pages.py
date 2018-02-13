c89faf1f53ad026255324cded10abf3237e36bdb
Jeremy Hooke <jez@stulk.com>
2018-02-13 16:19:39 +1100

    Include ODC version in footer

--- cubedash/_pages.py
+++ cubedash/_pages.py
@@ -17,8 +17,7 @@ app.register_blueprint(_product.bp)
 @app.route('/about')
 def about_page():
     return flask.render_template(
-        'about.html',
-        datacube_version=datacube.__version__
+        'about.html'
     )
 
 
@@ -44,6 +43,7 @@ def inject_globals():
         products=product_summaries,
         grouped_products=grouped_product_summarise,
         current_time=datetime.utcnow(),
+        datacube_version=datacube.__version__,
     )
 
 
