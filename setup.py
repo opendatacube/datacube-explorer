cb0b4315e62cb197a2d328a57dfb4c4411b06603
Jeremy Hooke <jez@stulk.com>
2018-02-16 16:00:52 +1100

    Require Python 3.5+

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     name="dea-dashboard",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    python_requires=">=3.5.2",
     url="https://github.com/data-cube/dea-dashboard",
     author="Geoscience Australia",
     packages=find_packages(),
