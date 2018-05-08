f6dc245c0ed9db767bd900872b8e94c05d0dd633
Jeremy Hooke <jez@stulk.com>
2018-05-08 14:26:59 +1000

    NCI loader: single log, skip completed steps

--- deployment/load-nci-dump.bash
+++ deployment/load-nci-dump.bash
@@ -3,12 +3,42 @@
 set -eu
 umask 0022
 
-echo "Starting restore $(date)"
 
-dump_id="$(date +%Y%m%d)"
+# Optional first argument is day to load (eg. "yesterday")
+dump_id="$(date "-d${1:-today}" +%Y%m%d)"
 psql_args="-h dea-pipeline-dev.cfeq4wxgcaui.ap-southeast-2.rds.amazonaws.com -U dea_db_admin"
 dump_file="/data/tmp/105-${dump_id}-datacube.pgdump"
 app_dir="/var/www/dea-dashboard"
+
+summary_dir="archive/${dump_id}"
+dbname="nci_${dump_id}"
+
+log_file="${summary_dir}/restore-$(date +'%dT%H%M').log"
+
+echo "======================="
+echo "Loading dump: ${dump_id}"
+echo "      dbname: ${dbname}"
+echo "         app: ${app_dir}"
+echo "        args: ${psql_args}"
+echo "         log: ${app_dir}/${log_file}"
+echo "======================="
+echo " in 5, 4, 3..."
+sleep 5
+
+cd "${app_dir}"
+mkdir -p "${summary_dir}"
+exec > "${log_file}"
+exec 2>&1
+
+function finish {
+    echo "Exiting $(date)"
+}
+trap finish EXIT
+
+echo "Starting restore $(date)"
+# Print local lowercase variables into log
+echo "Vars:"
+(set -o posix; set) | grep -e '^[a-z_]\+=' | sed 's/^/    /'
 
 if [ ! -e ${dump_file} ];
 then
@@ -19,28 +49,19 @@ then
 	set +x
 fi
 
-cd "${app_dir}"
-summary_dir="archive/${dump_id}"
-dbname="nci_${dump_id}"
+if psql ${psql_args} -lqtA | grep -q "^$dbname|";
+then 
+	echo "DB exists"
+else
+	createdb ${psql_args} "$dbname"
 
-echo "======================="
-echo "Loading dump: ${dump_id}"
-echo "      dbname: ${dbname}"
-echo "         app: ${app_dir}"
-echo "        args: ${psql_args}"
-echo "in 5..4..3..."
-echo "======================="
-sleep 5
 
-createdb ${psql_args} "$dbname"
-
-mkdir -p "${summary_dir}"
-
-# TODO: the dump has "create extension" statements which will fail (but that's ok here)
-echo "Restoring"
-# "no data for failed tables": when postgis extension fails to (re)initialise, don't populate its data
-# owner, privileges and tablespace are all NCI-specific.
-pg_restore -v --no-owner --no-privileges --no-tablespaces --no-data-for-failed-tables ${psql_args} -d "${dbname}" -j 4 "${dump_file}" &>> "${summary_dir}/restore-${dump_id}-$(date +'%dT%H%M').log"
+	# TODO: the dump has "create extension" statements which will fail (but that's ok here)
+	echo "Restoring"
+	# "no data for failed tables": when postgis extension fails to (re)initialise, don't populate its data
+	# owner, privileges and tablespace are all NCI-specific.
+	pg_restore -v --no-owner --no-privileges --no-tablespaces --no-data-for-failed-tables ${psql_args} -d "${dbname}" -j 4 "${dump_file}" || true
+fi
 
 
 # Hygiene
@@ -60,7 +81,7 @@ db_database: ${dbname}
 
 echo "Summary gen"
 # We randomise the product list to more evenly space large/small products between workers
-<"${summary_dir}/all-products.txt" sort -R | parallel --line-buffer -m -j4 /opt/conda/bin/python -m cubedash.generate --summaries-dir "${summary_dir}" &>> "${summary_dir}/gen-$(date +'%dT%H%M').log"
+<"${summary_dir}/all-products.txt" sort -R | parallel --line-buffer -m -j4 /opt/conda/bin/python -m cubedash.generate --summaries-dir "${summary_dir}"
 
 # Switch to new summaries
 ln -snf "${summary_dir}" product-summaries
