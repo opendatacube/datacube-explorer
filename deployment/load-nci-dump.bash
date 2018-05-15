69542e6191d7767244d8f0f86773abda6a111e0c
Jeremy Hooke <jez@stulk.com>
2018-05-15 11:39:29 +1000

    Clearer logging to dump loader

--- deployment/load-nci-dump.bash
+++ deployment/load-nci-dump.bash
@@ -9,7 +9,7 @@ export TZ="Australia/Sydney"
 # Optional first argument is day to load (eg. "yesterday")
 dump_id="$(date "-d${1:-today}" +%Y%m%d)"
 psql_args="-h dea-pipeline-dev.cfeq4wxgcaui.ap-southeast-2.rds.amazonaws.com -U dea_db_admin"
-dump_file="/data/tmp/105-${dump_id}-datacube.pgdump"
+dump_file="/data/nci/105-${dump_id}-datacube.pgdump"
 app_dir="/var/www/dea-dashboard"
 
 summary_dir="archive/${dump_id}"
@@ -32,24 +32,30 @@ mkdir -p "${summary_dir}"
 exec > "${log_file}"
 exec 2>&1
 
+
+function log_info {
+    printf '### %s\n' "$@"
+}
+
 function finish {
-    echo "Exiting $(date)"
+    log_info "Exiting $(date)"
 }
 trap finish EXIT
 
-echo "Starting restore $(date)"
+log_info "Starting restore $(date)"
 # Print local lowercase variables into log
-echo "Vars:"
+log_info "Vars:"
 (set -o posix; set) | grep -e '^[a-z_]\+=' | sed 's/^/    /'
 
 if psql ${psql_args} -lqtA | grep -q "^$dbname|";
 then 
-	echo "DB exists"
+	log_info "DB exists"
 else
 	if [ ! -e ${dump_file} ];
 	then
 		# Fetch new one
-		echo "Downloading backup from NCI. If there's no credentials, you'll have to do this manually and rerun:"
+		log_info "Downloading backup from NCI. If there's no credentials, you'll have to do this manually and rerun:"
+		# Our public key is whitelisted in lpgs to scp the latest backup (only)
 		set -x
 		scp "lpgs@r-dm.nci.org.au:/g/data/v10/agdc/backup/archive/105-${dump_id}-datacube.pgdump" "${dump_file}"
 		set +x
@@ -59,13 +65,13 @@ else
 
 
 	# TODO: the dump has "create extension" statements which will fail (but that's ok here)
-	echo "Restoring"
+	log_info "Restoring"
 	# "no data for failed tables": when postgis extension fails to (re)initialise, don't populate its data
 	# owner, privileges and tablespace are all NCI-specific.
 	pg_restore -v --no-owner --no-privileges --no-tablespaces --no-data-for-failed-tables ${psql_args} -d "${dbname}" -j 4 "${dump_file}" || true
 
 	# Hygiene
-	echo "Vacuuming"
+	log_info "Vacuuming"
 	psql ${psql_args} "${dbname}" -c "vacuum analyze;"
 fi
 
@@ -80,31 +86,31 @@ echo "
 db_database: ${dbname}
 " > datacube.conf
 
-echo "Restarting deadash (with updated dataset information)"
+log_info "Restarting deadash (with updated dataset information)"
 sudo systemctl restart deadash
 
-echo "Summary gen"
+log_info "Summary gen"
 # We randomise the product list to more evenly space large/small products between workers
 <"${summary_dir}/all-products.txt" sort -R | parallel --line-buffer -m -j4 /opt/conda/bin/python -m cubedash.generate --summaries-dir "${summary_dir}" || true
 
 if [ ! -e "${summary_dir}/timeline.json" ];
 then
-	echo "Summary gen failure: no overall summary"
+	log_info "Summary gen failure: no overall summary"
 	return 1
 fi
 
-echo "Switching to new summaries"
+log_info "Switching to new summaries"
 # Switch to new summaries
 ln -snf "${summary_dir}" product-summaries
 
-echo "Restarting deadash (with updated summaries)"
+log_info "Restarting deadash (with updated summaries)"
 sudo systemctl restart deadash
 
-echo "Warming caching"
+log_info "Warming caching"
 # Not strictly necessary, but users will see the new data sooner
 bash /data/warm-cache.sh
 
 [ -e "${dump_file}" ] && rm -v "${dump_file}"
 
-echo "All Done $(date) ${summary_dir}"
+log_info "All Done $(date) ${summary_dir}"
 
