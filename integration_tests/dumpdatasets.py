cd9e14bcadd3210a52614f194a5b5395093977c8
Jeremy Hooke <jez@stulk.com>
2018-05-25 16:24:33 +1000

    Allow dataset_dump to random sample

--- integration_tests/dumpdatasets.py
+++ integration_tests/dumpdatasets.py
@@ -11,19 +11,40 @@ import gzip
 import yaml
 
 from datacube.model import Range
+import random
 
 
-def dump_datasets(dc: Datacube, path: Path, **query):
+def _sample(iterable, sample_count):
+    """
+    Choose a random sampling of items from an iterator.
+
+    (you will get Nones if sample_count is less than iterable length)
+    """
+    result = [None] * sample_count
+    for i, item in enumerate(iterable):
+        if i < sample_count:
+            result[i] = item
+        else:
+            j = int(random.random() * (i + 1))
+            if j < sample_count:
+                result[j] = item
+    return result
+
+
+def dump_datasets(dc: Datacube, path: Path, dataset_sample_fraction=0.3, **query):
     total_count = dc.index.datasets.count(**query)
 
     if path.exists():
         raise ValueError(f"Path exists: {path}")
 
     product_name = query.get('product') or 'datasets'
-    msg = f'Dumping {total_count} {product_name} (with their sources)'
-    with click.progressbar(dc.index.datasets.search(**query),
-                           length=total_count,
-                           label=msg) as progress:
+    sample_count = int(total_count * dataset_sample_fraction)
+    msg = f'Dumping {sample_count} of {total_count} {product_name} (with their sources)'
+
+    with click.progressbar(
+            _sample(dc.index.datasets.search(**query), sample_count),
+            length=sample_count,
+            label=msg) as progress:
         with gzip.open(path, 'w') as f:
             yaml.dump_all(
                 (dc.index.datasets.get(d.id, include_sources=True).metadata_doc
@@ -35,23 +56,29 @@ def dump_datasets(dc: Datacube, path: Path, **query):
             )
 
 
+TEST_DATA_DIR = Path(__file__).parent / 'data'
+
 if __name__ == '__main__':
     with Datacube() as dc:
         dump_datasets(
             dc,
-            Path('ls8-nbar-albers-recent.yaml.gz'),
+            TEST_DATA_DIR / 'ls8-nbar-albers-sample.yaml.gz',
+            dataset_sample_fraction=0.1,
             product='ls8_nbar_albers',
-            time=Range(datetime(2017, 4, 15), datetime(2017, 5, 15))
-        )
-        dump_datasets(
-            dc,
-            Path('ls8-nbar-scene-2017.yaml.gz'),
-            product='ls8_nbar_scene',
-            time=Range(datetime(2017, 1, 1), datetime(2018, 1, 1))
+            time=Range(datetime(2017, 4, 15), datetime(2017, 5, 15)),
 
         )
         dump_datasets(
             dc,
-            Path('low-tide-comp-20p.yaml.gz'),
-            product='low_tide_comp_20p'
+            TEST_DATA_DIR / 'ls8-nbar-scene-sample-2017.yaml.gz',
+            dataset_sample_fraction=0.2,
+            product='ls8_nbar_scene',
+            time=Range(datetime(2017, 1, 1), datetime(2018, 1, 1)),
+        )
+        # Huuge amount of lineage.
+        dump_datasets(
+            dc,
+            TEST_DATA_DIR / 'low-tide-comp-20p.yaml.gz',
+            dataset_sample_fraction=0.1,
+            product='low_tide_comp_20p',
         )
