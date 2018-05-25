8d588dbb1c3c03422fb941ba79dd0546b7aecb77
Jeremy Hooke <jez@stulk.com>
2018-05-25 17:01:50 +1000

    Add albers product generation tests

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from pathlib import Path
 
+from cubedash import logs
 from datacube.scripts.dataset import create_dataset, load_rules_from_types
 from datacube.utils import read_documents
 
@@ -8,19 +9,34 @@ pytest_plugins = "digitalearthau.testing"
 
 TEST_DATA_DIR = Path(__file__).parent / 'data'
 
-_NBAR_SCENE_DUMP_PATH = TEST_DATA_DIR / 'ls8-nbar-scene-sample-2017.yaml.gz'
-
 
 # TODO: scope="session", as index is expensive to populate
 # Needs the upstream dea_index to allow it.
 @pytest.fixture
-def populated_index(dea_index):
-    ls8_nbar_scene = dea_index.products.get_by_name('ls8_nbar_scene')
+def populated_scene_index(dea_index):
+    _populate_from_dump(
+        dea_index,
+        'ls8_nbar_scene',
+        TEST_DATA_DIR / 'ls8-nbar-scene-sample-2017.yaml.gz'
+    )
+    return dea_index
 
+
+@pytest.fixture
+def populated_albers_index(dea_index):
+    _populate_from_dump(
+        dea_index,
+        'ls8_nbar_albers',
+        TEST_DATA_DIR / 'ls8-nbar-albers-sample.yaml.gz'
+    )
+    return dea_index
+
+
+def _populate_from_dump(dea_index, expected_type: str, dump_path: Path):
+    ls8_nbar_scene = dea_index.products.get_by_name(expected_type)
     dataset_count = 0
-
     rules = load_rules_from_types(dea_index)
-    for _, doc in read_documents(_NBAR_SCENE_DUMP_PATH):
+    for _, doc in read_documents(dump_path):
         created = dea_index.datasets.add(
             create_dataset(doc, None, rules),
         )
@@ -28,4 +44,12 @@ def populated_index(dea_index):
         assert created.type.name == ls8_nbar_scene.name
         dataset_count += 1
 
-    return dea_index
+    print(f"Populated {dataset_count} of {expected_type}")
+    return dataset_count
+
+
+@pytest.fixture(autouse=True, scope='session')
+def init_logs(pytestconfig):
+    logs.init_logging(
+        verbose=pytestconfig.getoption('verbose') > 0
+    )
--- integration_tests/test_summarise.py
+++ integration_tests/test_summarise.py
@@ -1,3 +1,4 @@
+import pytest
 from datetime import datetime
 from dateutil.tz import tzutc
 
@@ -7,8 +8,9 @@ from cubedash.summary import FileSummaryStore
 from datacube.model import Range
 
 
-def test_calc_month_scenes(populated_index, tmpdir):
-    store = FileSummaryStore(populated_index, Path(tmpdir))
+@pytest.mark.skip
+def test_calc_scene_summary(populated_scene_index, tmpdir):
+    store = FileSummaryStore(populated_scene_index, Path(tmpdir))
 
     s = store.calculate_summary(
         'ls8_nbar_scene',
@@ -30,8 +32,6 @@ def test_calc_month_scenes(populated_index, tmpdir):
     # (note: this should be a separate test method, but we don't want to create
     # the populated_index multiple times. The fixture scope needs to be changed
     # upstream to allow it.)
-    store = FileSummaryStore(populated_index, Path(tmpdir))
-
     s = store.update(
         'ls8_nbar_scene',
         year=None,
@@ -39,12 +39,68 @@ def test_calc_month_scenes(populated_index, tmpdir):
         day=None,
     )
 
+    print(repr(s))
     assert s.dataset_count == 2478
     assert s.footprint_count == 2478
     assert s.newest_dataset_creation_time == datetime(
         2018, 1, 10, 3, 11, 38, tzinfo=tzutc()
     )
-    print(len(s.dataset_counts))
     assert len(s.dataset_counts) == 365
     # ??
     # assert s.period == 'day'
+
+
+def test_calc_albers_summary(populated_albers_index, tmpdir):
+    store = FileSummaryStore(populated_albers_index, Path(tmpdir))
+
+    # Should not exist yet.
+    summary = store.get(
+        'ls8_nbar_scene',
+        year=None,
+        month=None,
+        day=None,
+    )
+    assert summary is None
+    summary = store.get(
+        'ls8_nbar_scene',
+        year=2017,
+        month=None,
+        day=None,
+    )
+    assert summary is None
+
+    # Calculate overall summary
+    summary = store.get_or_update(
+        'ls8_nbar_scene',
+        year=2017,
+        month=None,
+        day=None,
+    )
+    print(repr(summary))
+    assert summary.dataset_count == 617
+    assert summary.footprint_count == 617
+    assert summary.newest_dataset_creation_time == datetime(
+        2017, 7, 11, 4, 32, 11, tzinfo=tzutc()
+    )
+    # Data spans 61 days in 2017
+    assert len(summary.dataset_counts) == 61
+
+    # get_or_update should now return the cached copy.
+    cached_s = store.get_or_update(
+        'ls8_nbar_scene',
+        year=2017,
+        month=None,
+        day=None,
+    )
+    assert cached_s.summary_gen_time is not None
+    assert cached_s.summary_gen_time == summary.summary_gen_time
+    assert cached_s.dataset_count == summary.dataset_count
+
+    # No datasets in 2018
+    summary = store.get_or_update(
+        'ls8_nbar_scene',
+        year=2018,
+        month=None,
+        day=None,
+    )
+    assert summary.dataset_count == 0, "There should be no datasets in 2018"
