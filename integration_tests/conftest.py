be27517a5cfa5a1bc4075a1830da4b95b4d6a6c9
Jeremy Hooke <jez@stulk.com>
2018-05-25 15:39:47 +1000

    Add scene summary integration test

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -1,2 +1,31 @@
+import pytest
+from pathlib import Path
+
+from datacube.scripts.dataset import create_dataset, load_rules_from_types
+from datacube.utils import read_documents
 
 pytest_plugins = "digitalearthau.testing"
+
+TEST_DATA_DIR = Path(__file__).parent / 'data'
+
+_NBAR_SCENE_DUMP_PATH = TEST_DATA_DIR / 'ls8-nbar-scene-sample-2017.yaml.gz'
+
+
+# TODO: scope="session", as index is expensive to populate
+# Needs the upstream dea_index to allow it.
+@pytest.fixture
+def populated_index(dea_index):
+    ls8_nbar_scene = dea_index.products.get_by_name('ls8_nbar_scene')
+
+    dataset_count = 0
+
+    rules = load_rules_from_types(dea_index)
+    for _, doc in read_documents(_NBAR_SCENE_DUMP_PATH):
+        created = dea_index.datasets.add(
+            create_dataset(doc, None, rules),
+        )
+
+        assert created.type.name == ls8_nbar_scene.name
+        dataset_count += 1
+
+    return dea_index
