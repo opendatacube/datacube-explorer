1228ee61925f0581d3081ebc26fc60dd9b0dc274
Jeremy Hooke <jez@stulk.com>
2018-08-28 17:14:07 +1000

    Fix timeline test: some empty records not stored

--- cubedash/summary/_stores.py
+++ cubedash/summary/_stores.py
@@ -341,9 +341,9 @@ class SummaryStore:
                 return
 
             timezone = tz.gettz(self._summariser.grouping_time_zone)
-            if datetime(year, month or 1, day or 1, tzinfo=timezone) < product.time_earliest:
+            if datetime(year, month or 12, day or 28, tzinfo=timezone) < product.time_earliest:
                 return
-            if datetime(year, month or 12, day or 28, tzinfo=timezone) > product.time_latest:
+            if datetime(year, month or 1, day or 1, tzinfo=timezone) > product.time_latest:
                 return
 
         self._put(product_name, year, month, day, summary)
