b0a9cca05d68035bf0cad9b12f203c38373e7324
Damien Ayers <damien@omad.net>
2020-02-26 15:40:41 +1100

    Silly code reformatting

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -1,10 +1,11 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
 from ._pages import app
-from pkg_resources import get_distribution, DistributionNotFound
 
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
     # package is not installed
-    __version__ = 'Unknown/Not Installed'
+    __version__ = "Unknown/Not Installed"
 
 __all__ = (app, __version__)
