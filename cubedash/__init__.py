dbe4e0c3be3c6cd3ef5b1c7b19716e7e65751821
whatnick <tisham@whatnick.com>
2020-02-27 02:53:07 +0000

    #81 Move Werkzeug fix to __init__

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -1,5 +1,10 @@
+import werkzeug
+
 from ._pages import app
 from ._version import get_versions
+
+werkzeug.cached_property = werkzeug.utils.cached_property
+
 
 __version__ = get_versions()["version"]
 del get_versions
