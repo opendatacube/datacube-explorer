a5de9f0eda7fe31fa4fb37fe7d704342d3c96211
Damien Ayers <damien@omad.net>
2020-02-28 12:17:00 +1100

    Fix version number display in UI

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -1,11 +1,8 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
-from ._pages import app
-
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = "Unknown/Not Installed"
+    from ._version import version as __version__
+except ImportError:
+    __version__ = 'Unknown/Not Installed'
+    
+from ._pages import app
 
 __all__ = (app, __version__)
