6c860283e91e987bfaccb4da60e1857c1a6aa7df
Jeremy Hooke <jez@stulk.com>
2020-03-02 10:11:59 +1100

    Black shouldn't test autogenerated code

--- cubedash/__init__.py
+++ cubedash/__init__.py
@@ -1,8 +1,8 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = 'Unknown/Not Installed'
-    
+    __version__ = "Unknown/Not Installed"
+
 from ._pages import app
 
 __all__ = (app, __version__)
