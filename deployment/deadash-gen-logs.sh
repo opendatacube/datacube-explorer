9fca2fd40058582e5627528c9a9f9bbc9f49c694
Alex Leith <alexgleith@gmail.com>
2020-04-27 11:43:01 +1000

    Removing config files

--- deployment/deadash-gen-logs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-log_dir="/var/log/nginx"
-out_file="/var/www/dea-dashboard/cubedash/static/access.html"
-
-zcat "${log_dir}/"access.log*.gz | /bin/goaccess --log-format=COMBINED -a -o "${out_file}" "${log_dir}/access.log"  -
