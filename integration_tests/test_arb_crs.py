34f308a8dc73ac3db7ca9a75b152a572fed69f8a
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-06-15 03:44:46 +0000

    #127 Add CRS inference code

---
+++ integration_tests/test_arb_crs.py
@@ -0,0 +1,18 @@
+"""
+Test utility method for creating valid EPSG code based CRS from
+possible WKT String
+"""
+from cubedash._utils import infer_crs
+
+TEST_CRS_RAW = """
+GEOGCS["GEOCENTRIC DATUM of AUSTRALIA",DATUM["GDA94",SPHEROID["GRS80",6378137,298.257222101]],
+PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]
+"""
+
+
+def test_crs_infer_fail():
+    assert infer_crs("") is None
+
+
+def test_crs_infer_pass():
+    assert infer_crs(TEST_CRS_RAW) == "EPSG:4283"
