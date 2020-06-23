abae6a37ee8fd016b58bd59e7fbdab4f01bc24bb
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-06-23 05:50:58 +0000

    Add 404 page

--- cubedash/_pages.py
+++ cubedash/_pages.py
@@ -357,3 +357,7 @@ def product_list_text():
     return Response(
         "\n".join(_model.STORE.list_complete_products()), content_type="text/plain"
     )
+
+@app.errorhandler(404)
+def page_not_found(error):
+   return utils.render('404.html', title = '404'), 404
