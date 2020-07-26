2b7bdb0ac110ffb7633e510aeaa5b80485b4232b
Tisham Dhar <whatnickd@gmail.com>
2020-07-26 11:11:17 +1000

    Extra blank lines
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- integration_tests/test_sentry.py
+++ integration_tests/test_sentry.py
@@ -5,6 +5,7 @@ from flask.testing import FlaskClient
 import cubedash
 from integration_tests.asserts import get_html
 
+
 @pytest.fixture
 def sentry_client(empty_client : FlaskClient) -> FlaskClient:
     cubedash.app.config["SENTRY_CONFIG"] = {
@@ -13,6 +14,7 @@ def sentry_client(empty_client : FlaskClient) -> FlaskClient:
     }
     return empty_client
 
+    
 def test_sentry(sentry_client : FlaskClient):
     html = get_html(sentry_client, "/")
     assert html
