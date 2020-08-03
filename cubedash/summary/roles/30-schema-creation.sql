20e1f59455d9248016c520401d53be84034fe1e3
Jeremy Hooke <jez@stulk.com>
2020-08-03 15:31:53 +1000

    cubedash schema should be owned by explorer-owner

--- cubedash/summary/roles/30-schema-creation.sql
+++ cubedash/summary/roles/30-schema-creation.sql
@@ -7,7 +7,7 @@
 begin;
 
 -- Give them a schema, which the Explorer Owner can populate.
-create schema if not exists cubedash;
+create schema if not exists cubedash authorization explorer_owner;
 grant create on schema cubedash to explorer_owner;
 
 commit;
