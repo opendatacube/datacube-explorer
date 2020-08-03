2244c7822fca6cd960c915ef7dcfaf6b98a15d63
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-08-03 07:30:07 +0000

    #173 Delay load prometheus metrics

--- cubedash/gunicorn_config.py
+++ cubedash/gunicorn_config.py
@@ -1,9 +1,12 @@
 """Gunicorn config for Prometheus internal metrics
 """
 import os
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 
 def child_exit(server, worker):
     if os.environ.get("prometheus_multiproc_dir", False):
+        from prometheus_flask_exporter.multiprocess import (
+            GunicornInternalPrometheusMetrics,
+        )
+
         GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
