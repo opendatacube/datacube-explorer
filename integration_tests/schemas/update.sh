6276fd088632569bf433755afee89a31f03eb7bf
Jeremy Hooke <jez@stulk.com>
2020-09-24 10:53:09 +1000

    More stac validation updates

--- integration_tests/schemas/update.sh
+++ integration_tests/schemas/update.sh
@@ -24,15 +24,17 @@ subfolder="stac-spec-${stac_version}"
 wget https://github.com/radiantearth/stac-spec/archive/${stac_tag}.tar.gz
 tar -xvf ${stac_tag}.tar.gz --wildcards "${subfolder}/*/json-schema/*.json"
 rm ${stac_tag}.tar.gz
+mv ${subfolder} "schemas.stacspec.org/${stac_tag}"
 rm -f stac
-ln -s ${subfolder} stac
+ln -s "schemas.stacspec.org/${stac_version}" stac
 
 api_subfolder="stac-api-spec-${stac_api_tag}"
 wget https://github.com/radiantearth/stac-api-spec/archive/${stac_api_tag}.tar.gz
 tar -xvf ${stac_api_tag}.tar.gz --wildcards "${api_subfolder}/*/json-schema/*.json"
 rm ${stac_api_tag}.tar.gz
+mv ${api_subfolder} "schemas.stacspec.org/${stac_api_tag}"
 rm -f stac-api
-ln -s ${api_subfolder} stac-api
+ln -s "schemas.stacspec.org/${stac_api_tag}" stac-api
 
 # The ItemCollection was removed from core stac, but is used by stac-api.
 cd stac/item-spec/json-schema
