8151648287a42d33d01bc076f4358170ca702358
Jeremy Hooke <jez@stulk.com>
2020-10-07 12:47:14 +1100

    Tests shouldn't depend on upstream product count
    The DEA repo can add and remove them at any time... such as right now.

--- integration_tests/test_page_loads.py
+++ integration_tests/test_page_loads.py
@@ -300,12 +300,14 @@ def test_view_metadata_type(client: FlaskClient, populated_index: Index):
     assert "ls8_nbar_albers" in products_using_it
 
 
-def test_about_page(client: FlaskClient):
+def test_about_page(client: FlaskClient, populated_index: Index):
     html: HTML = get_html(client, "/about")
 
     assert html.find(".product-name", containing="wofs_albers")
-    assert "80 Products" in html.text
-    assert len(html.find(".data-table tbody tr")) == 80
+
+    product_count = len(list(populated_index.products.get_all()))
+    assert f"{product_count} Products" in html.text
+    assert len(html.find(".data-table tbody tr")) == product_count
 
 
 @pytest.mark.skip(reason="TODO: fix out-of-date range return value")
