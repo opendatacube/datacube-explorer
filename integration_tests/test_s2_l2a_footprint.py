37b3e2f237b0f1c15b25f4e2efee10e0f9409473
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-11-11 03:53:38 +0000

    add test case

--- integration_tests/test_s2_l2a_footprint.py
+++ integration_tests/test_s2_l2a_footprint.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 import pytest
 from dateutil.tz import tzutc
-from flask import Response
 from flask.testing import FlaskClient
 
 from cubedash.summary import SummaryStore
@@ -28,19 +27,19 @@ def populate_index(dataset_loader, module_dea_index):
     path, s2_product_doc = list(
         read_documents(TEST_DATA_DIR / "esa_s2_l2a.product.yaml")
     )[0]
+    dataset_count = 0
     product_ = module_dea_index.products.from_doc(s2_product_doc)
     module_dea_index.products.add(product_)
     create_dataset = Doc2Dataset(module_dea_index)
     for _, s2_dataset_doc in read_documents(TEST_DATA_DIR / "s2_l2a-sample.yaml"):
-        print(s2_dataset_doc)
-        try:
-            dataset, err = create_dataset(
-                s2_dataset_doc, "file://example.com/test_dataset/"
-            )
-            created = module_dea_index.datasets.add(dataset)
-            assert created.type.name == "s2_l2a"
-        except AttributeError as ae:
-            print(ae)
+        dataset, err = create_dataset(
+            s2_dataset_doc, "file://example.com/test_dataset/"
+        )
+        assert dataset is not None, err
+        created = module_dea_index.datasets.add(dataset)
+        assert created.type.name == "s2_l2a"
+        dataset_count += 1
+    assert dataset_count == 2
     return module_dea_index
 
 
@@ -51,13 +50,13 @@ def test_s2_l2a_summary(run_generate, summary_store: SummaryStore):
         dataset_count=1,
         footprint_count=1,
         time_range=Range(
-            begin=datetime(2017, 9, 30, 14, 30, tzinfo=tzutc()),
-            end=datetime(2017, 10, 31, 14, 30, tzinfo=tzutc()),
+            begin=datetime(2019, 5, 31, 14, 30, tzinfo=tzutc()),
+            end=datetime(2019, 6, 30, 14, 30, tzinfo=tzutc()),
         ),
-        newest_creation_time=datetime(2018, 7, 26, 23, 49, 25, 684_327, tzinfo=tzutc()),
+        newest_creation_time=datetime(2019, 6, 20, 11, 57, 34, tzinfo=tzutc()),
         timeline_period="day",
-        timeline_count=31,
-        crses={"EPSG:32753"},
+        timeline_count=30,
+        crses={"EPSG:32627"},
         size_bytes=0,
     )
 
@@ -66,23 +65,16 @@ def test_product_audit(unpopulated_client: FlaskClient, run_generate):
     run_generate()
     client = unpopulated_client
 
-    res = get_html(client, "/product-audit/?timings")
-    # print(res.html)
+    res = get_html(client, "/product-audit/")
+    print(res.html)
 
-    largest_footprint_size = res.find(".footprint-size .search-result")
-    assert len(largest_footprint_size) == 2
-
-    largest_product_footprint = (
-        largest_footprint_size[0].find(".product-name", first=True).text
+    assert (
+        res.find(".unavailable-metadata .search-result .product-name", first=True).text
+        == "s2_l2a"
     )
-    largest_val = largest_footprint_size[0].find(".size-value", first=True).text
-    # They're both the same :/
-    assert largest_product_footprint in ("s2a_ard_granule", "s2a_level1c_granule")
-    assert largest_val == "181.6B"
-
-    assert len(res.find(".unavailable-metadata .search-result")) == 2
-
-    res: Response = client.get("/product-audit/day-times.txt")
-    plain_timing_results = res.data.decode("utf-8")
-    print(plain_timing_results)
-    assert '"s2a_ard_granule"\t8\t' in plain_timing_results
+    assert (
+        res.find(
+            ".unavailable-metadata .search-result .missing-footprint", first=True
+        ).attrs["title"]
+        == "0 of 1 missing footprint"
+    )
