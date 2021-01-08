c39c58e50ea1a44f770190034a1913d3b25eb236
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-01-08 02:05:24 +0000

    Valid test case

--- integration_tests/test_filter_geom.py
+++ integration_tests/test_filter_geom.py
@@ -2,13 +2,67 @@
 Unit test for re-cursive geometry filtering
 """
 import json
+from pathlib import Path
 
-from shapely.geometry import shape
+from shapely.geometry import shape, MultiPolygon, Polygon
+import shapely.wkt
+import csv
+import pytest
 
-from cubedash.summary._model import _filter_geom
+from cubedash.summary._model import _filter_geom, _polygon_chain
 
+TEST_DATA_DIR = Path(__file__).parent / "data"
+
+
+class Valid_geometries:
+    def __init__(self, footprint_geometry):
+        self.footprint_geometry = footprint_geometry
+
+
+@pytest.fixture
+def testing_polygon():
+    sample_geometry_file = open(TEST_DATA_DIR / "unary_union_fail_sample.txt")
+    line_c = 0
+    shapely_polygon = []
+    for line in sample_geometry_file:
+        if len(line) > 1:
+            line_c += 1
+            poly = shapely.wkt.loads(line)
+            shapely_polygon.append(Valid_geometries(poly))
+    assert len(shapely_polygon) == 56
+    return shapely_polygon
 
 def test_filter_geom():
     assert _filter_geom([]) == []
     geom = shape(json.loads('{"type": "Point", "coordinates": [0.0, 0.0]}'))
     assert _filter_geom([geom])
+
+
+def test_nested_exception(testing_polygon):
+    """
+    simulating the behaviour in _model.py
+    """
+    geometry_union = None
+    try:
+        geometry_union = shapely.ops.unary_union(
+            [ ele.footprint_geometry for ele in testing_polygon ]
+            )
+    except ValueError:
+        assert geometry_union is None
+        try:
+            geometry_union = shapely.ops.unary_union(
+                [ ele.footprint_geometry.buffer(0.00) for ele in testing_polygon ]
+                )
+            assert geometry_union is None
+
+        except ValueError:
+               polygonlist =  _polygon_chain(testing_polygon)
+               assert type(polygonlist) is list
+               assert len(polygonlist) == 472
+               filtered_geom = _filter_geom(polygonlist)
+               assert len(filtered_geom) == 222
+               geometry_union = (
+                   shapely.ops.unary_union(filtered_geom)
+               )
+
+               assert geometry_union.is_valid
