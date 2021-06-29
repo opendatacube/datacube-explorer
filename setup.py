90caa564c64f4c7357fb1e6046ad5168e5865d87
Alex Leith <alexgleith@gmail.com>
2021-06-29 16:57:30 +1000

    Bump EO Datasets to 0.21.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "click",
         "dataclasses>=0.6;python_version<'3.7'",
         "datacube>=1.8",
-        "eodatasets3>=0.17.0",
+        "eodatasets3>=0.21.1",
         "fiona",
         "flask",
         "Flask-Caching",
