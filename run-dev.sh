2dc6119157f3b0b395d201e9660d8801b4ed1a2b
Jeremy Hooke <jez@stulk.com>
2021-09-14 11:00:51 +1000

    Remove old run scripts
    They're replaced by cubedash-run

--- run-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Run single threaded server with debugger & reloading enabled.
-export FLASK_APP=cubedash
-export FLASK_DEBUG=1
-export PYTHONPATH=${PYTHONPATH}:`pwd`
-
-python -m flask run -p 8080
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-gunicorn \
-    -w 5 \
-    --timeout 300 \
-    --worker-class="egg:meinheld#gunicorn_worker" \
-    cubedash:app
-
