b86cd9bb9d6b0538c1c4ca61b255de4389d4ddc5
Jeremy Hooke <jez@stulk.com>
2021-10-08 11:20:16 +1100

    Improve CRS inference
    The test is broken on newer docker container -- built-in WKT strings
    are slightly different.
    
    Strangely, it was sorting ascending and picking the first. ... so the least
    good match? But this worked because the test data used a completely different capitalisation to the real CRS, so they didn't match at all.
    
    Changed them to:
    
    1. Compare without case sensitivity.
    2. Compare total matches, rather than ratio. WKTs are very diff.
       lengths, and we want to match their strings of names/numbers,
       not penalize for missing sections (which is common).
    3. Pick the best match, not the least

--- integration_tests/test_arb_crs.py
+++ integration_tests/test_arb_crs.py
@@ -2,17 +2,26 @@
 Test utility method for creating valid EPSG code based CRS from
 possible WKT String
 """
+from pyproj import CRS
+
 from cubedash._utils import infer_crs
 
+# This CRS was embedded in DEA's gamma-ray product definition.
 TEST_CRS_RAW = """
 GEOGCS["GEOCENTRIC DATUM of AUSTRALIA",DATUM["GDA94",SPHEROID["GRS80",6378137,298.257222101]],
 PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]
 """
 
 
-def test_crs_infer_fail():
+def test_crs_infer_has_match_floor():
+    """Don't match something that's too different"""
     assert infer_crs("") is None
 
 
 def test_crs_infer_pass():
     assert infer_crs(TEST_CRS_RAW) == "epsg:4283"
+
+
+def test_crs_infers_itself():
+    """Sanity check: something should match itself!"""
+    assert infer_crs(CRS.from_epsg(4326).to_wkt()) == "epsg:4326"
