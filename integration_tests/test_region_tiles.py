ac635a9574f8bbe75c3c0379980e74e15ec5b191
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-06-16 01:57:15 +0000

    add more test case for region tile handling

---
+++ integration_tests/test_region_tiles.py
@@ -0,0 +1,136 @@
+"""
+Tests that indexes DEA C3 Summary products region tiles
+"""
+from pathlib import Path
+
+import pytest
+from datacube.index.hl import Doc2Dataset
+from datacube.utils import read_documents
+from flask.testing import FlaskClient
+
+from integration_tests.asserts import check_dataset_count, get_html
+
+TEST_DATA_DIR = Path(__file__).parent / "data"
+
+
+@pytest.fixture(scope="module", autouse=True)
+def populate_wo_index(dataset_loader, module_dea_index):
+    """
+    Index populated with example datasets. Assumes our tests wont modify the data!
+
+    It's module-scoped as it's expensive to populate.
+    """
+    dataset_count = 0
+    create_dataset = Doc2Dataset(module_dea_index)
+    for _, s2_dataset_doc in read_documents(TEST_DATA_DIR / "ga_ls_wo_fq_nov_mar_3-sample.yaml"):
+        try:
+            dataset, err = create_dataset(
+                s2_dataset_doc, "file://example.com/test_dataset/"
+            )
+            assert dataset is not None, err
+            created = module_dea_index.datasets.add(dataset)
+            assert created.type.name == "ga_ls_wo_fq_nov_mar_3"
+            dataset_count += 1
+        except AttributeError as ae:
+            assert dataset_count == 5
+            print(ae)
+    assert dataset_count == 5
+    return module_dea_index
+
+
+def test_wo_summary_product(client: FlaskClient):
+    html = get_html(client, "/ga_ls_wo_fq_nov_mar_3")
+
+    check_dataset_count(html, 5)
+
+
+def test_wo_region_dataset_count(client: FlaskClient):
+    html = get_html(client, "/product/ga_ls_wo_fq_nov_mar_3/regions/x11y46")
+
+    search_results = html.find(".search-result a")
+    assert len(search_results) == 5
+
+
+# Test where region_code is defined in metadata but all are the same
+
+@pytest.fixture(scope="module", autouse=True)
+def populate_landcover_index(dataset_loader, module_dea_index):
+    """
+    Index populated with example datasets. Assumes our tests wont modify the data!
+
+    It's module-scoped as it's expensive to populate.
+    """
+    dataset_count = 0
+    create_dataset = Doc2Dataset(module_dea_index)
+    for _, s2_dataset_doc in read_documents(TEST_DATA_DIR / "ga_ls_landcover_class_cyear_2-sample.yaml"):
+        try:
+            dataset, err = create_dataset(
+                s2_dataset_doc, "file://example.com/test_dataset/"
+            )
+            assert dataset is not None, err
+            created = module_dea_index.datasets.add(dataset)
+            assert created.type.name == "ga_ls_landcover_class_cyear_2"
+            dataset_count += 1
+        except AttributeError as ae:
+            assert dataset_count == 3
+            print(ae)
+    assert dataset_count == 3
+    return module_dea_index
+
+
+def test_landcover_summary_product(client: FlaskClient):
+    html = get_html(client, "/ga_ls_landcover_class_cyear_2")
+
+    check_dataset_count(html, 3)
+
+
+def test_landcover_region_dataset_count(client: FlaskClient):
+    html = get_html(client, "/product/ga_ls_landcover_class_cyear_2/regions/au")
+
+    search_results = html.find(".search-result a")
+    assert len(search_results) == 3
+
+# Test where region_code is not defined in metadata
+
+
+@pytest.fixture(scope="module", autouse=True)
+def populate_tmad_index(dataset_loader, module_dea_index):
+    """
+    Index populated with example datasets. Assumes our tests wont modify the data!
+
+    It's module-scoped as it's expensive to populate.
+    """
+    dataset_count = 0
+    create_dataset = Doc2Dataset(module_dea_index)
+    for _, s2_dataset_doc in read_documents(TEST_DATA_DIR / "ls5_nbart_tmad_annual-sample.yaml"):
+        try:
+            dataset, err = create_dataset(
+                s2_dataset_doc, "file://example.com/test_dataset/"
+            )
+            assert dataset is not None, err
+            created = module_dea_index.datasets.add(dataset)
+            assert created.type.name == "ls5_nbart_tmad_annual"
+            dataset_count += 1
+        except AttributeError as ae:
+            assert dataset_count == 2
+            print(ae)
+    assert dataset_count == 2
+    return module_dea_index
+
+
+def test_tmad_summary_product(client: FlaskClient):
+    html = get_html(client, "/ls5_nbart_tmad_annual")
+
+    check_dataset_count(html, 2)
+
+
+def test_tmad_region_dataset_count(client: FlaskClient):
+    html = get_html(client, "product/ls5_nbart_tmad_annual/regions/-14_-25")
+
+    search_results = html.find(".search-result a")
+    assert len(search_results) == 1
+
+    html = get_html(client, "product/ls5_nbart_tmad_annual/regions/8_-36")
+
+    search_results = html.find(".search-result a")
+    assert len(search_results) == 1
