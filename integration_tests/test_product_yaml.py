ffbb8478f98bda59b444977344afb51da7aa09e9
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-07-05 06:55:44 +0000

    combine test

--- integration_tests/test_product_yaml.py
+++ integration_tests/test_product_yaml.py
@@ -5,6 +5,7 @@ import datacube.scripts.cli_app
 import datacube.ui.click
 
 import tempfile
+import pytest
 
 
 def test_product_yaml_with_scientific_notation_is_valid(client: FlaskClient, clirunner):
@@ -47,43 +48,33 @@ def test_wagl_product_yaml_is_valid(client: FlaskClient, clirunner):
     assert result.exit_code == 0
 
 
-def test_low_complication_product_yaml_is_valid(client: FlaskClient, clirunner):
+@pytest.fixture()
+def product_yaml_from_raw(client):
+    f = tempfile.NamedTemporaryFile(suffix='.yaml', delete=False)
+
     response = client.get("/products/ls5_fc_albers.odc-product.yaml", follow_redirects=True)
     assert response.content_type == 'text/yaml'
 
-    f = tempfile.NamedTemporaryFile(suffix='.yaml', delete=False)
     f.write(response.data)
-    assert type(f.name) == str
 
+    response = client.get("/products/dsm1sv10.odc-product.yaml", follow_redirects=True)
+    assert response.content_type == 'text/yaml'
+
+    f.write(response.data)
+    return f.name
+
+
+def test_update_product(product_yaml_from_raw, clirunner):
     result = clirunner(
         datacube.scripts.cli_app.cli,
         [
             "product",
             "update",
-            f.name,
+            product_yaml_from_raw,
         ],
         expect_success=False,
     )
 
-    assert result.output.endswith('Updated "ls5_fc_albers"\n')
-    assert result.exit_code == 0
-
-
-def test_simple_product_yaml_is_valid(client: FlaskClient, clirunner):
-    response = client.get("/products/dsm1sv10.odc-product.yaml", follow_redirects=True)
-    assert response.content_type == 'text/yaml'
-
-    f = tempfile.NamedTemporaryFile(suffix='.yaml', delete=False)
-    f.write(response.data)
-
-    result = clirunner(
-        datacube.scripts.cli_app.cli,
-        [
-            "product",
-            "update",
-            f.name,
-        ]
-    )
-
-    assert result.output.endswith('Updated "dsm1sv10"\n')
+    assert 'Updated "ls5_fc_albers"\n' in result.output
+    assert 'Updated "dsm1sv10"\n' in result.output
     assert result.exit_code == 0
