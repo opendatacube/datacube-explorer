942069f7f4b9846fa1c6df8593c905e248acea4c
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-07-11 01:04:56 +0000

    attach request.path to sister sites

--- integration_tests/test_configurable_page_elements.py
+++ integration_tests/test_configurable_page_elements.py
@@ -35,3 +35,34 @@ def test_sister_sites(app_configured_client: FlaskClient):
         "#sister-site-menu ul li"
     )
     assert len(sister_instances) == 2
+
+    for sister_instance in sister_instances:
+        assert '/about' in sister_instance.find(
+            "a.sister-link", first=True
+        ).attrs["href"]
+
+
+def test_sister_sites_request_path(app_configured_client: FlaskClient):
+    html = get_html(app_configured_client, "/products/ga_ls5t_ard_3")
+
+    sister_instances = html.find(
+        "#sister-site-menu ul li"
+    )
+    assert len(sister_instances) == 2
+
+    for sister_instance in sister_instances:
+        assert '/products/ga_ls5t_ard_3' in sister_instance.find(
+            "a.sister-link", first=True
+        ).attrs["href"]
+
+    html = get_html(app_configured_client, "/products/ga_ls5t_ard_3/datasets")
+
+    sister_instances = html.find(
+        "#sister-site-menu ul li"
+    )
+    assert len(sister_instances) == 2
+
+    for sister_instance in sister_instances:
+        assert '/products/ga_ls5t_ard_3/datasets' in sister_instance.find(
+            "a.sister-link", first=True
+        ).attrs["href"]
