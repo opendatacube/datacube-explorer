def9b9808452ee08d21ff366e5ea4cda4c1dbbad
Ubuntu <azureuser@tai-ol-dev.syvicaqdxptelj5t5axwfigvif.fx.internal.cloudapp.net>
2022-09-16 16:58:06 +0000

    Adding setting.env.py file to controlling loading TAI theme.

---
+++ settings.env.py
@@ -0,0 +1,5 @@
+# How many days of recent datasets to show on the "/arrivals" page?
+CUBEDASH_DEFAULT_ARRIVALS_DAY_COUNT = 14
+
+# Which theme to use (in the cubedash/themes folder)
+CUBEDASH_THEME = 'tai'
