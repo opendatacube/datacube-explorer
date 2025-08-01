dfce68b91cd9c11e61a8487bee885a68398ca528
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-08-01 23:59:28 +0200

    Fix geojson schema URLs

--- integration_tests/test_stac.py
+++ integration_tests/test_stac.py
@@ -129,13 +129,13 @@ def _web_reference(ref: str):
     """
     A reference to a schema via a URL
 
-    eg http://geojson.org/schemas/Features.json'
+    e.g https://geojson.org/schema/Feature.json
     """
     if not is_url(ref):
         raise NoSuchResource(f"Expected URL? Got {ref!r}")
     (scheme, netloc, offset, params, query, fragment) = urllib.parse.urlparse(ref)
     # We used `wget -r` to download the remote schemas locally.
-    # It puts into hostname/path folders by default. Eg. 'geojson.org/schema/Feature.json'
+    # It puts into hostname/path folders by default. E.g 'geojson.org/schema/Feature.json'
     path = _SCHEMA_BASE / f"{netloc}{offset}"
     if not path.exists():
         if ALLOW_INTERNET:
