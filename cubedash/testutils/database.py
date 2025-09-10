6a5332bf60e6165db8f1aa77a795d69daab424bf
Damien Ayers <damien@omad.net>
2025-09-10 10:17:59 +1000

    Docker Postgres improvements

--- cubedash/testutils/database.py
+++ cubedash/testutils/database.py
@@ -50,15 +50,45 @@ def postgresql_server():
     else:
         client = docker.from_env()
         logger.info("Starting PostGIS Docker Container", image=POSTGIS_IMAGE)
+        postgres_args = [
+            # Memory settings (helpful even with tmpfs)
+            "-c",
+            "shared_buffers=256MB",
+            "-c",
+            "work_mem=4MB",
+            "-c",
+            "maintenance_work_mem=64MB",
+            # Skip WAL archiving and replication setup
+            "-c",
+            "wal_level=minimal",
+            "-c",
+            "max_wal_senders=0",
+            "-c",
+            "archive_mode=off",
+            # These are redundant with tmpfs but included for completeness
+            "-c",
+            "fsync=off",
+            "-c",
+            "synchronous_commit=off",
+            "-c",
+            "full_page_writes=off",
+        ]
         container = client.containers.run(
             POSTGIS_IMAGE,
+            command=postgres_args,
             auto_remove=True,
             remove=True,
             detach=True,
             environment={
                 "POSTGRES_PASSWORD": "explorer_test_password",
                 "POSTGRES_USER": "explorer_test_user",
-                "POSTGRES_DB": "explorer_test_db"
+                "POSTGRES_DB": "explorer_test_db",
+                "POSTGRES_INITDB_ARGS": "--no-sync",  # Skip initdb's fsync for faster initial setup
+                "PGDATA": "/tmp/explorertest/data",
+            },
+            tmpfs={
+                "/tmp/explorertest": "rw,noexec,nosuid,noatime,nodiratime,size=128m"  # Use RAM drive for storage
+                # ,size=1g
             },
             ports={"5432/tcp": None},
         )
@@ -72,10 +102,7 @@ def postgresql_server():
             # From the documentation for the postgres docker image. The value of POSTGRES_USER
             # is used for both the user and the default database.
             db_url = f"postgresql://explorer_test_user:explorer_test_password@127.0.0.1:{host_port}/explorer_test_db"
-            logger.info(
-                "Docker container started",
-                db_url=db_url
-            )
+            logger.info("Docker container started", db_url=db_url)
             yield db_url
         finally:
             logger.info("Removing PostGIS container")
@@ -104,7 +131,9 @@ def odc_db(
     if postgresql_server == GET_DB_FROM_ENV:
         yield ODCConfig()[env_name]
     else:
-        log = logger.bind(db_url=postgresql_server, worker_id=worker_id, testrun_uid=testrun_uid)
+        log = logger.bind(
+            db_url=postgresql_server, worker_id=worker_id, testrun_uid=testrun_uid
+        )
         test_database_name = f"{request.path.stem}_{env_name}"
         # Wait for PostgreSQL Server to start up
         while True:
@@ -115,15 +144,24 @@ def odc_db(
 
                 # assert not test_database_name.endswith('postgis')
                 # if test_database_name.endswith('postgis'):
-                    # import web_pdb; web_pdb.set_trace()
+                # import web_pdb; web_pdb.set_trace()
 
                 try:
                     with conn.cursor() as cur:
                         log.info("Creating database", db_name=test_database_name)
-                        cur.execute(sql.SQL("CREATE DATABASE {db_name}").format(db_name=sql.Identifier(test_database_name)))
+                        cur.execute(
+                            sql.SQL("CREATE DATABASE {db_name}").format(
+                                db_name=sql.Identifier(test_database_name)
+                            )
+                        )
                 except psycopg2.errors.DuplicateDatabase:
-                    log.warning("Database already exists, continuing", db_name=test_database_name)
-                    import web_pdb; web_pdb.set_trace()
+                    log.warning(
+                        "Database already exists, continuing",
+                        db_name=test_database_name,
+                    )
+                    import web_pdb
+
+                    web_pdb.set_trace()
                     # breakpoint()
                 finally:
                     conn.close()
@@ -132,16 +170,18 @@ def odc_db(
                 log.info("Waiting for PostgreSQL to become available")
                 time.sleep(1)
 
-        new_postgresql_url = postgresql_server.replace("explorer_test_db", test_database_name)
+        new_postgresql_url = postgresql_server.replace(
+            "explorer_test_db", test_database_name
+        )
 
         log = log.bind(db_url=new_postgresql_url)
-        log.info('Creating postgis extension')
+        log.info("Creating postgis extension")
         conn = psycopg2.connect(new_postgresql_url)
         # Prevent starting a transaction, since 'create extension' cannot run inside a transaction
         conn.set_session(autocommit=True)
         with conn:
             with conn.cursor() as cur:
-                cur.execute('CREATE EXTENSION postgis')
+                cur.execute("CREATE EXTENSION postgis")
         conn.close()
 
         # Create a temporary configuration file with our database URL and index driver name
@@ -174,12 +214,14 @@ def cfg_env(odc_db, env_name: str) -> ODCEnvironment:
     """Provide a :class:`ODCEnvironment` configured with suitable config file paths."""
     # Need to request `odc_db` since it exports an environment variable specifying which
     # configuration file to use. So this fixture *must* run after that fixture.
-    odc_env =  ODCConfig().get_environment(env_name)
+    odc_env = ODCConfig().get_environment(env_name)
     assert odc_env.db_url is not None
-    logger.info("Prepping ODCConfig",
-                env_name=env_name,
-                odc_config_path_from_env=os.environ['ODC_CONFIG_PATH'],
-                odc_env_url=odc_env.db_url)
+    logger.info(
+        "Prepping ODCConfig",
+        env_name=env_name,
+        odc_config_path_from_env=os.environ["ODC_CONFIG_PATH"],
+        odc_env_url=odc_env.db_url,
+    )
     return odc_env
 
 
@@ -197,7 +239,9 @@ def odc_test_db(odc_db: str, cfg_env: ODCEnvironment) -> Generator[Datacube]:
         Ready to go Datacube Object
 
     """
-    logger.info("Initialising ODC Schema for testing", odc_db=odc_db, cfg_db_url=cfg_env.db_url)
+    logger.info(
+        "Initialising ODC Schema for testing", odc_db=odc_db, cfg_db_url=cfg_env.db_url
+    )
     index = index_connect(cfg_env, validate_connection=False)
     # With permissions causes trouble when run multiple times on the same database
     # Although, maybe not, since there should be a separate database every time.
@@ -270,7 +314,9 @@ def _remove_postgis_dynamic_indexes() -> None:
     """Clear any dynamically created postgis indexes from the schema."""
     # Our normal indexes start with "ix_", dynamic indexes with "dix_"
     for table in pgis_core.METADATA.tables.values():
-       table.indexes.intersection_update([i for i in table.indexes if not i.name.startswith('dix_')])
+        table.indexes.intersection_update(
+            [i for i in table.indexes if not i.name.startswith("dix_")]
+        )
     # Dynamic indexes disabled.
 
 
@@ -287,9 +333,11 @@ def auto_odc_db(odc_test_db: Datacube, request: pytest.FixtureRequest):
     The fixture makes available a dict, keyed by name, counting the number of datasets
     added, not including derivatives.
     """
-    log = logger.bind(current_db=odc_test_db.index.environment.db_url,
-                      current_dc=str(odc_test_db),
-                      module=request.module.__name__)
+    log = logger.bind(
+        current_db=odc_test_db.index.environment.db_url,
+        current_dc=str(odc_test_db),
+        module=request.module.__name__,
+    )
     log.info("Preparing to load data into Test ODC Database")
     odc_test_db.index.metadata_types.check_field_indexes(
         allow_table_lock=True, rebuild_indexes=False, rebuild_views=True
